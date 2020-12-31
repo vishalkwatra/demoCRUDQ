a8b12b6d4736fc0b3193443675d5a967efd20aaf
Vishal.Kwatra <Vishal.Kwatra@IN2465447W4.ey.net>
2020-12-31 12:56:48 +0530

    CRUDQ with Rest Controller

--- demoCRUD1/src/main/java/com/ey/hr/lms/code/Controller/StudentController.java
+++ demoCRUD1/src/main/java/com/ey/hr/lms/code/Controller/StudentController.java
@@ -5,30 +5,63 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ey.hr.lms.code.Entities.Student;
 
 @RestController
 public class StudentController {
-	
+
 	@Autowired
 	StudentRepo studentRepoObj;
-	
+
 	@RequestMapping("/students")
-	public List<Student> giveAllStudent(){
+	public List<Student> giveAllStudent() {
 		return studentRepoObj.findAll();
 	}
-	
+
 	@RequestMapping("/students/{id}")
 	public Student giveAllStudent(@PathVariable long id) {
 		Optional<Student> studentRecord = studentRepoObj.findById(id);
-		
-		if(!studentRecord.isPresent()) {
+
+		if (!studentRecord.isPresent()) {
+			return new Student();
+		}
+
+		return studentRecord.get();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/addNewStudent")
+	public Student addNewStudent(@RequestBody Student studentObj) {
+		Student studentReturn = studentRepoObj.save(studentObj);
+		return studentReturn;
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/updateStudent/{id}")
+	public Student updateStudent(@RequestBody Student studentObj, @PathVariable long id) {
+		Optional<Student> studentRecord = studentRepoObj.findById(id);
+
+		if (!studentRecord.isPresent()) {
 			return new Student();
 		}
 		
+		return studentRepoObj.save(studentObj);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/deleteStudent/{id}")
+	public Student Student(@PathVariable long id) {
+		Optional<Student> studentRecord = studentRepoObj.findById(id);
+
+		if (!studentRecord.isPresent()) {
+			return new Student();
+		}
+		
+		studentRepoObj.deleteById(id);
+		
 		return studentRecord.get();
 	}
+
 }
